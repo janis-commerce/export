dd684872d3e40adaa51643492d02304ca0743cd0
Nataniel López <natanielopz@gmail.com>
2020-04-15 10:09:43 -0300

    Added statuses tests for export model

--- tests/model-export.js
+++ tests/model-export.js
@@ -12,6 +12,17 @@ describe('Models', () => {
 			it('Should return the correct table', () => {
 				assert.strictEqual(ModelExport.table, 'exports');
 			});
+
+			it('Should return the correct statuses', () => {
+				assert.deepStrictEqual(ModelExport.statuses, {
+					pending: 'pending',
+					processing: 'processing',
+					processed: 'processed',
+					sending: 'sending',
+					sent: 'sent',
+					sendingError: 'sending-error'
+				});
+			});
 		});
 	});
 });
