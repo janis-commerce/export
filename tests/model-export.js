f9f60e995c795ce5bc5caa095374ea61a8715b03
maguimarijuan <magali.marijuan@fizzmod.com>
2020-06-02 14:46:00 -0300

    add new model state

--- tests/model-export.js
+++ tests/model-export.js
@@ -16,6 +16,7 @@ describe('Models', () => {
 			it('Should return the correct statuses', () => {
 				assert.deepStrictEqual(ModelExport.statuses, {
 					pending: 'pending',
+					processing: 'processing',
 					processed: 'processed',
 					sent: 'sent',
 					sendingError: 'sending-error'
